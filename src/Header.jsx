777dcb645e4d01232d5b3f36a3e35e8b0871725c
gordonmaloney <gordonmaloney@gmail.com>
2022-12-28 16:21:26 +0000

    .

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
-  maxWidth: '95%',
+  maxWidth: '90%',
   bgcolor: "background.paper",
   border: "1px solid #000",
   boxShadow: 24,
